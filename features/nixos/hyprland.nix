5d255b21924d1bf80c4963f2d935f0f19fc9a5ae
Cu3PO42 <cu3po42@gmail.com>
2023-09-23 21:35:43 +0200

    feat(nixos): only enable Hyprland nVidia patches on nVidia systems

--- features/nixos/hyprland.nix
+++ features/nixos/hyprland.nix
@@ -1,10 +1,10 @@
-{inputs, ...}: {
+{config, inputs, ...}: {
   imports = [
     inputs.hyprland.nixosModules.default
   ];
 
   programs.hyprland = {
     enable = true;
-    nvidiaPatches = true;
+    nvidiaPatches = config.copper.feature.nvidia.enable;
   };
 }
