7ab11c4308ac767af0f4a22afccf023a307e2e39
Cu3PO42 <cu3po42@gmail.com>
2023-12-23 22:25:02 +0100

    feat(home): add themes for CLI apps

--- themes/Catppuccin-Latte/default.nix
+++ themes/Catppuccin-Latte/default.nix
@@ -46,4 +46,35 @@
   rofi = {
     files."theme.rasi" = ./rofi.rasi;
   };
+
+  fish.theme = {
+    file = "${pkgs.fetchFromGitHub {
+      owner = "catppuccin";
+      repo = "fish";
+      rev = "91e6d6721362be05a5c62e235ed8517d90c567c9";
+      hash = "sha256-l9V7YMfJWhKDL65dNbxaddhaM6GJ0CFZ6z+4R6MJwBA=";
+    }}/themes/Catppuccin Latte.theme";
+    name = "Catppuccin Latte";
+  };
+
+  starship.palette = {
+    file = pkgs.fetchFromGitHub {
+      owner = "catppuccin";
+      repo = "starship";
+      rev = "3e3e54410c3189053f4da7a7043261361a1ed1bc";
+      hash = "sha256-soEBVlq3ULeiZFAdQYMRFuswIIhI9bclIU8WXjxd7oY=";
+    }
+    + /palettes/latte.toml;
+    name = "catppuccin_latte";
+  };
+
+  bat.theme = {
+    src = pkgs.fetchFromGitHub {
+      owner = "catppuccin";
+      repo = "bat";
+      rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
+      hash = "sha256-6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
+    };
+    file = "Catppuccin-latte.tmTheme";
+  };
 }
