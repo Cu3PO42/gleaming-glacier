c647bd4c0af8ea9b80a569490ee9283daa680daa
Cu3PO42 <cu3po42@gmail.com>
2024-01-09 11:55:39 +0100

    feat(home): configure swaylock and swayidle via HM modules
    This also resolves the issue that the system would go to sleep after unlock.

--- features/home-manager/nixos/swaylock.nix
+++ features/home-manager/nixos/swaylock.nix
@@ -1,11 +1,31 @@
-{pkgs, ...}: {
-  wayland.windowManager.hyprland.settings = {
-    # lock & turn off monitor after 20 mins, suspend after 30 mins // install swayidle
-    exec-once = ["swayidle -w timeout 1200 'swaylock; hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on' timeout 1800 'systemctl suspend'"];
+{config, lib, pkgs, ...}: {
+  programs.swaylock = {
+    enable = true;
+    package = pkgs.swaylock-effects;
+    settings = {
+      clock = true;
+      indicator = true;
+      grace = 5;
+      image = lib.mkIf config.copper.swim.enable "${config.xdg.stateHome}/swim/active/wallpaper";
+      daemonize = true;
+    };
   };
 
-  home.packages = with pkgs; [
-    swaylock-effects
-    swayidle
-  ];
+  services.swayidle = {
+    enable = true;
+    systemdTarget = "hyprland-session.target";
+    timeouts = [
+      { timeout = 600; command = "${pkgs.systemd}/bin/loginctl lock-session"; }
+      {
+        timeout = 1200;
+        command = "${config.wayland.windowManager.hyprland.finalPackage}/bin/hyprctl dispatch dpms off";
+        resumeCommand = "${config.wayland.windowManager.hyprland.finalPackage}/bin/hyprctl dispatch dpms on";
+      }
+      { timeout = 1800; command = "${pkgs.systemd}/bin/systemctl suspend"; }
+    ];
+    events = [
+      { event = "before-sleep"; command = "${pkgs.systemd}/bin/loginctl lock-session";}
+      { event = "lock"; command = "${lib.getExe config.programs.swaylock.package}"; }
+    ];
+  };
 }
