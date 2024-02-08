861f523e4b61d26f30e64b39741e7a5b5a8e3268
Cu3PO42 <cu3po42@gmail.com>
2024-02-08 22:19:25 +0100

    feat: update system template for new structure

--- templates/system/flake.nix
+++ templates/system/flake.nix
@@ -10,51 +10,7 @@
     copper.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    copper,
-    ...
-  } @ inputs:
-    with copper.lib; let
-      # This is a simple helper function that for a given function f, runs
-      # `f sysetm` for any system in the list and stores the result in an
-      # attribute called `system`.
-      # This is used to abstract over systems for all attrtibutes that support
-      # multiple.
-      forAllSystems = nixpkgs.lib.genAttrs ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
-    in {
-      # Copy the formatter, this is required so that `nix fmt` works.
-      inherit (copper) formatter;
-
-      # Copy the plate tool, which we might need to deploy a server.
-      packages = forAllSystems (s: {inherit (copper.packages.${s}) plate;});
-      # Copy the scripts that simplify usage of this template.
-      apps = forAllSystems (s: {inherit (copper.apps.${s}) generate bootstrap;});
-      # Copy the devShell that can be used for initial setup
-      devShells = forAllSystems (s: {inherit (copper.devShells) default;});
-
-      # Just as in Copper's flake, load all modules from their respective folders.
-      # This implicilty imports all .nix files in the module folders!
-      nixosModules = loadModules ./. "nixos";
-      homeModules = loadModules ./. "home-manager";
-      darwinModules = loadModules ./. "darwin";
-
-      # Just as in Copper's flake, load all host and user configurations from
-      # their respective folders. Additionally, pass in any modules defined in
-      # this flake. Modules from Copper's flake are also included. Pass
-      # `withCopperModules = false` to change that behavior.
-      nixosConfigurations = loadNixos {
-        dir = ./hosts/nixos;
-        extraModules = builtins.attrValues self.outputs.nixosModules;
-      };
-      homeConfigurations = loadHome {
-        dir = ./users;
-        extraModules = builtins.attrValues self.outputs.homeModules;
-      };
-      darwinConfigurations = loadDarwin {
-        dir = ./hosts/darwin;
-        extraModules = builtins.attrValues self.outputs.darwinModules;
-      };
-    };
+  outputs = inputs: inputs.copper.lib.mkGleamingFlake inputs ./. "myprefix" (flakeModules: {
+    # Your custom flake attrributes go here!
+  });
 }
