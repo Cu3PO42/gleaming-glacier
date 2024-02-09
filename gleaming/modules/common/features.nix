efd16f14e18ec02bcc38a5028bc7f4486ed0a1ec
Cu3PO42 <cu3po42@gmail.com>
2024-02-09 10:48:35 +0100

    fix(flake): fix gleaming features' module when no features exist

--- gleaming/modules/common/features.nix
+++ gleaming/modules/common/features.nix
@@ -23,16 +23,22 @@ with lib; let
     mapAttrs (_: enableOrStep) grouped;
 in {
   options = {
-    ${gleaming.basename}.features = mkOption {
-      type = with types; listOf str;
-      default = [];
-      example = ["cli" "rust" "fish"];
-      description = ''
-        This is simply a convenience option to enable a number of features
-        from a Gleaming Glacier-based config. For every element `mod` that
-        is inculded here, the option
-        `${gleaming.basename}.features.''${mod}.enable = true`; will be set.
-      '';
+    ${gleaming.basename} = {
+      features = mkOption {
+        type = with types; listOf str;
+        default = [];
+        example = ["cli" "rust" "fish"];
+        description = ''
+          This is simply a convenience option to enable a number of features
+          from a Gleaming Glacier-based config. For every element `mod` that
+          is inculded here, the option
+          `${gleaming.basename}.features.''${mod}.enable = true`; will be set.
+        '';
+      };
+
+      # Make sure that assigning to `feature` is valid even if we don't define
+      # any features.
+      feature = {};
     };
   };
 
