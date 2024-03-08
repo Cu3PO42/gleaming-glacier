cb4b7bfb637ca2ff05f2404d6c67d91c8b857c03
Cu3PO42 <cu3po42@gmail.com>
2024-03-08 19:36:51 +0100

    feat(home): make sleep in hypridle optional

--- features/home-manager/nixos/hyprlock.nix
+++ features/home-manager/nixos/hyprlock.nix
@@ -1,36 +1,44 @@
-{config, lib, origin, pkgs, ...}: {
+{config, lib, origin, pkgs, ...}: with lib; let
+  cfg = config.copper.feature.nixos.hyprlock;
+in {
+  featureOptions = {
+    suspend.enable = mkEnableOption "Automatic sleep via Hypridle";
+  };
+
   imports = [
     origin.inputs.hypridle.homeManagerModules.default
     origin.inputs.hyprlock.homeManagerModules.default
   ];
 
-  programs.hyprlock = {
-    enable = true;
-    backgrounds = [
-      {
-        monitor = "";
-        path = "${config.xdg.stateHome}/swim/active/wallpaper";
-      }
-    ];
+  config = {
+    programs.hyprlock = {
+      enable = true;
+      backgrounds = [
+        {
+          monitor = "";
+          path = "${config.xdg.stateHome}/swim/active/wallpaper";
+        }
+      ];
 
-    general = {
-      grace = 5;
+      general = {
+        grace = 5;
+      };
     };
-  };
 
-  services.hypridle = {
-    enable = true;
-    beforeSleepCmd = "${pkgs.systemd}/bin/loginctl lock-session";
-    lockCmd = lib.getExe config.programs.hyprlock.package;
+    services.hypridle = {
+      enable = true;
+      beforeSleepCmd = "${pkgs.systemd}/bin/loginctl lock-session";
+      lockCmd = lib.getExe config.programs.hyprlock.package;
 
-    listeners = [
-      {
-        timeout = 300;
-        onTimeout = "${pkgs.systemd}/bin/loginctl lock-session";
-      }
-      {
+      listeners = [
+        {
+          timeout = 300;
+          onTimeout = "${pkgs.systemd}/bin/loginctl lock-session";
+        }
+      ] ++ (optional cfg.suspend.enable {
         timeout = 600;
-      }
-    ];
+        onTimeout = "${pkgs.systemd}/bin/systemctl suspend";
+      });
+    };
   };
 }
