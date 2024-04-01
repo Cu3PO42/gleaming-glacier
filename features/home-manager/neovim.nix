a6a2099aec1d4d5d8375a2e61cd1e54bf7957334
Cu3PO42 <cu3po42@gmail.com>
2024-04-01 23:38:02 +0200

    fix(home): allow our LazyVim config to work without explicit symlinks

--- features/home-manager/neovim.nix
+++ features/home-manager/neovim.nix
@@ -3,7 +3,7 @@
   config,
   lib,
   ...
-}: let
+}: with lib; let
   hasGit = config.programs.git.enable;
 in {
   # TODO: figure out why smartindent is on (at least for nix files)
@@ -44,5 +44,34 @@ in {
     '')
   ];
 
+  imports = [
+    (mkIf config.copper.file.symlink.enable {
   copper.file.config."nvim" = "config/nvim";
+    })
+    (mkIf (!config.copper.file.symlink.enable) {
+      xdg.configFile."nvim".source = pkgs.stdenvNoCC.mkDerivation {
+        name = "nvim-config";
+        src = ../../config/nvim;
+        buildPhase = ''
+          rm lazy-lock.json lazyvim.json
+          ln -s ${config.xdg.stateHome}/nvim/lazy-lock.json ./lazy-lock.json
+          ln -s ${config.xdg.stateHome}/nvim/lazyvim.json ./lazyvim.json
+        '';
+        installPhase = ''
+          mkdir $out
+          mv ./* $out
+        '';
+      };
+
+      home.activation.nvim = lib.hm.dag.entryAfter ["writeboundary"] ''
+        mkdir -p ${config.xdg.stateHome}/nvim
+        if [ ! -e ${config.xdg.stateHome}/nvim/lazy-lock.json ]; then
+          install -m 644 ${../../config/nvim/lazy-lock.json} ${config.xdg.stateHome}/nvim/lazy-lock.json
+        fi
+        if [ ! -e ${config.xdg.stateHome}/nvim/lazyvim.json ]; then
+          install -m 644 ${../../config/nvim/lazyvim.json} ${config.xdg.stateHome}/nvim/lazyvim.json
+        fi
+      '';
+    })
+  ];
 }
