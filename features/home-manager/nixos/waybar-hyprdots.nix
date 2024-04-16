f8e8add9d04a3ac77d274f2215facac2110e724e
Cu3PO42 <cu3po42@gmail.com>
2024-04-16 19:38:49 +0200

    feat(home): move Hyprdots' waybar confgen out of HM activation

--- features/home-manager/nixos/waybar-hyprdots.nix
+++ features/home-manager/nixos/waybar-hyprdots.nix
@@ -7,9 +7,13 @@
 }: {
   copper.feature.nixos.waybar.enable = true;
 
-  home.activation.waybarConfigGeneration = lib.hm.dag.entryBetween ["reloadSystemd"] ["linkGeneration"] ''
-    ${lib.getExe copper.packages.waybar-confgen-hyprdots}
-  '';
+  systemd.user.services.waybar = let
+    confgen = "${lib.getExe copper.packages.waybar-confgen-hyprdots}";
+  in {
+    Service.ExecStartPre = confgen;
+    Service.ExecReload = lib.mkForce confgen;
+  };
+
 
   home.packages = [copper.packages.waybar-confgen-hyprdots];
 
