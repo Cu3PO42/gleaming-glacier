d8ca011ac195d52a1758d58370751a45fceeb223
Cu3PO42 <cu3po42@gmail.com>
2024-04-16 21:05:28 +0200

    fix(nixos): explicitly use Qt5-based SDDM

--- features/nixos/sddm.nix
+++ features/nixos/sddm.nix
@@ -1,7 +1,10 @@
-{copper, ...}: {
-  services.xserver.displayManager.sddm = {
+{copper, pkgs, ...}: {
+  services.displayManager.sddm = {
     enable = true;
     theme = "corners";
+    # Explicitly use the Qt5 version of SDDM, because that is what the theme is
+    # compatible with.
+    package = pkgs.libsForQt5.sddm;
     # This is a fix for a huge onscreen keyboard appearing and hiding everything.
     settings.General.InputMethod = "";
   };
