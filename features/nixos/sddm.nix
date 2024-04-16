493782cdb855ce9d73afb15e5f1a7a2c89ddcec0
Cu3PO42 <cu3po42@gmail.com>
2024-04-16 23:42:24 +0200

    feat(nixos): display user icons in SDDM

--- features/nixos/sddm.nix
+++ features/nixos/sddm.nix
@@ -1,4 +1,4 @@
-{copper, pkgs, ...}: {
+{config, lib, copper, pkgs, ...}: {
   services.displayManager.sddm = {
     enable = true;
     theme = "corners";
@@ -7,6 +7,21 @@
     package = pkgs.libsForQt5.sddm;
     # This is a fix for a huge onscreen keyboard appearing and hiding everything.
     settings.General.InputMethod = "";
+    # SDDM theoretically supports getting user icons from AccountsService (which
+    # we have set up), but implements it poorly. Instead of actually querying
+    # AccountsService for the icon, it hardcodes the path where it will typically
+    # place the icons. Since we place them elsewhere, we need to set up icons for
+    # SDDM seperately.
+    settings.Theme.FacesDir = let
+      usersWithIcons = lib.filterAttrs (_: value: value.icon != null) config.users.users;
+      # Icons need to end in .face.icon
+      iconLinks = lib.mapAttrsToList (name: value: "ln -s ${value.icon} ${name}.face.icon") usersWithIcons;
+      icons = pkgs.runCommand "user-icons" {} ''
+        mkdir -p $out
+        cd $out
+        ${lib.concatStringsSep "\n" iconLinks}
+      '';
+    in "${icons}";
   };
 
   environment.systemPackages = [
