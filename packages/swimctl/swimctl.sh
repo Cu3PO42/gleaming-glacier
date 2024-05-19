b54c2e9690753ea46311464e26ddd660c93544b5
Cu3PO42 <cu3po42@gmail.com>
2024-05-19 22:48:23 +0200

    fix(home): correctly run wallpaper activation commands on theme switch

--- packages/swimctl/swimctl.sh
+++ packages/swimctl/swimctl.sh
@@ -6,19 +6,25 @@ SWIM_STATE_DIR="${XDG_STATE_HOME:-$HOME/.local/state}/swim"
 # Another option would be a symlink!
 WALLPAPER_DIR=$(jq -r '.wallpaperDirectory' "${SWIM_CONFIG_DIR}/config.json")
 
-# spellcheck disable=SC2046
-activateWallpaper() {
+updateWallpaper() {
     if ! [ -f "${WALLPAPER_DIR}/$1" ]; then
         echo "Wallpaper $1 does not exist"
         exit 1
     fi
 
+    ln -sf "${WALLPAPER_DIR}/$1" "${SWIM_STATE_DIR}/active/wallpaper"
+
+    activateWallpaper
+}
+
+# spellcheck disable=SC2046
+activateWallpaper() {
     local TRANS="${TRANSITION:-grow}"
     mapfile -t swwwArgs < <(jq -r '.extraSwwwArgs | .[]' "${SWIM_CONFIG_DIR}/config.json" | while read -r line; do
         eval "echo $line"
     done)
     swww \
-        img "$WALLPAPER_DIR/$1" \
+        img "${SWIM_STATE_DIR}/active/wallpaper" \
         --transition-bezier ".43,1.19,1,.4" \
         --transition-type "$TRANS" \
         --transition-duration 0.7 \
@@ -26,30 +32,28 @@ activateWallpaper() {
         --invert-y \
         "${swwwArgs[@]}"
 
-    ln -sf "${WALLPAPER_DIR}/$1" "${SWIM_STATE_DIR}/active/wallpaper"
-
-    WALLPAPER="${WALLPAPER_DIR}/$1" "$SWIM_CONFIG_DIR/activate"
+    WALLPAPER="${SWIM_STATE_DIR}/active/wallpaper" "$SWIM_CONFIG_DIR/activate"
 }
 
 case $1 in
     next)
         CURRENT_WALLPAPER="$(basename "$(readlink -f "${SWIM_STATE_DIR}/active/wallpaper")")"
         NEXT="$(find "$WALLPAPER_DIR/" -type f -printf '%P\n' | jq -nrR --arg current "$CURRENT_WALLPAPER" '[inputs] | .[(index($current) + 1) % length]')"
-        TRANSITION="grow" activateWallpaper "$NEXT"
+        TRANSITION="grow" updateWallpaper "$NEXT"
         ;;
     previous)
         CURRENT_WALLPAPER="$(basename "$(readlink -f "${SWIM_STATE_DIR}/active/wallpaper")")"
         NEXT="$(find "$WALLPAPER_DIR/" -type f -printf '%P\n'| jq -nrR --arg current "$CURRENT_WALLPAPER" '[inputs] | .[(index($current) + 0 - 1) % length]')"
-        TRANSITION="outer" activateWallpaper "$NEXT"
+        TRANSITION="outer" updateWallpaper "$NEXT"
         ;;
     select)
-        activateWallpaper "$2"
+        updateWallpaper "$2"
         ;;
     list)
         find "$WALLPAPER_DIR/" -type f -printf '%P\n'
         ;;
     activate)
-        swww img "$SWIM_STATE_DIR/active/wallpaper"
+        activateWallpaper
         ;;
     *)
         echo "Usage: swimctl <command>"
