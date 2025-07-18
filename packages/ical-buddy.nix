7922bca8a5bcce2e37d46e0d77c71d4c08eb244c
Cu3PO42 <cu3po42@gmail.com>
2025-07-18 13:41:54 +0200

    chore(pkgs): update ical-buddy for new apple-sdk structure

--- packages/ical-buddy.nix
+++ packages/ical-buddy.nix
@@ -2,17 +2,15 @@
 # https://github.com/Homebrew/homebrew-core/blob/2f089a6f55a7496248be39f4d883094fd79cbdc4/Formula/ical-buddy.rb
 # Originally written by yuanw at
 # https://github.com/yuanw/nix-home/blob/ab1ab8c8bdbc018d105766a71a5732ded37cf18f/overlays/ical-buddy.nix
-# Unfortunately, they did not expose the package in their flake, so I copied it over to mine.
+# Unfortunately, they did not expose the package in their flake, so I copied it over to mine and
+# have adjusted it since.
 {
   lib,
   stdenv,
   fetchFromGitHub,
   pkgs,
   ...
-}: let
-  frameworks = pkgs.darwin.apple_sdk.frameworks;
-in
-  stdenv.mkDerivation rec {
+}: stdenv.mkDerivation rec {
     pname = "icallBuddy";
     version = "1.10.1";
 
@@ -23,7 +21,7 @@ in
       hash = "sha256-ID3U7lAve3DHTHx2kCunwg1LWkIEIIpQrXkvmkvn/Mg=";
     };
 
-    buildInputs = [pkgs.perl frameworks.AppKit frameworks.Carbon frameworks.Cocoa frameworks.AddressBook frameworks.CalendarStore];
+    buildInputs = [pkgs.perl];
     buildPhase = ''
       # Allow native builds rather than only x86_64
       sed -i 's/-arch x86_64//g' Makefile
