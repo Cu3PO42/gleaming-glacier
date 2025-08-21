d398d0900eb13e50dc94a322a23ede51ecaa71e1
Cu3PO42 <cu3po42@gmail.com>
2025-08-21 23:31:22 +0200

    fix(pkgs): remove broken replaceDependencies package and replace it with upstream version

--- legacy-packages/replace-dependencies.nix
+++ legacy-packages/replace-dependencies.nix
@@ -1,5 +1,3 @@
 # This integrates a package that is currently sitting in nixpkgs PR #257234
-{callPackage, fetchurl, ...}: callPackage (fetchurl {
-  url = "https://raw.githubusercontent.com/alois31/nixpkgs/3700df871b4cf1895de0936cab0f521514a978bb/pkgs/build-support/replace-dependencies.nix";
-  hash = "sha256-RVl5yj5sgbblExT+Tgg9rhk/PXtd2So8RQ5VaVJptDo=";
-}) {}
+{replaceDependencies, lib, ...}:
+lib.warn "replaceDependencies is now included in upstream Nixpkgs, there is no need to rely on my packaged version anymore" replaceDependencies
