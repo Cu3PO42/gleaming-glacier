1e7b51c3246976181159890650151167ea51bec1
Cu3PO42 <cu3po42@gmail.com>
2025-10-06 10:59:04 +0200

    chore(pkgs): only show warning for replace-dependencies when it is used

--- legacy-packages/replace-dependencies.nix
+++ legacy-packages/replace-dependencies.nix
@@ -1,3 +1,3 @@
 # This integrates a package that is currently sitting in nixpkgs PR #257234
 {replaceDependencies, lib, ...}:
-lib.warn "replaceDependencies is now included in upstream Nixpkgs, there is no need to rely on my packaged version anymore" replaceDependencies
+args: lib.warn "replaceDependencies is now included in upstream Nixpkgs, there is no need to rely on my packaged version anymore" (replaceDependencies args)
